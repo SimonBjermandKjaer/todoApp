f0dc65f45795f97f72222dca0db73df40e69929c
SimonBjermandKjaer <sbk@it-minds.dk>
2021-09-17 10:17:49 +0200

    filer

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, './my-app/dist/my-app/')));
+app.use(express.static(path.join(__dirname, '/dist/my-app/')));
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', { root: './my-app/dist/my-app/' }
+    res.sendFile('index.html', { root: 'dist/my-app/' }
     );
 });
 app.listen(process.env.PORT || 8000);
