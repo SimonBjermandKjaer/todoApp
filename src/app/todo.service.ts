97f0ca463b5afebc9124f4f09bf73077d931c934
SimonBjermandKjaer <sbk@it-minds.dk>
2021-09-17 13:37:32 +0200

    deployment

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -13,9 +13,69 @@ const httpOptions = {
 
 export class Todo {
 
-    readonly APIUrl = "https://localhost:44351/api";
+    // readonly APIUrl = "https://localhost:44351/api";
 
-    readonly loginUrl = "https://localhost:44351/Account";
+    // readonly loginUrl = "https://localhost:44351/Account";
+
+    // public $todos = new BehaviorSubject<TodoClass[]>([])
+
+    // tokenHeader: HttpHeaders = new HttpHeaders;
+
+    // constructor(private http: HttpClient) { }
+
+    // setTokenHeader(): HttpHeaders {
+    //     return new HttpHeaders({ 'Authorization': 'Bearer ' + localStorage.getItem('jwt') });
+    // }
+
+    // getTodos(): Observable<TodoClass[]> {
+    //     this.tokenHeader = this.setTokenHeader();
+
+    //     return this.http.get<TodoClass[]>(this.APIUrl + '/Todos', { headers: this.tokenHeader }).pipe(tap((data) => {
+    //         this.$todos.next(data);
+    //     }));
+    // }
+
+    // postTodos(dataToSend: TodoClass): Observable<TodoClass[]> {
+    //     this.tokenHeader = this.setTokenHeader();
+    //     return this.http.post<TodoClass[]>(this.APIUrl + '/Todos', dataToSend, { headers: this.tokenHeader });
+    // }
+
+    // deleteTodos(id: number): Observable<TodoClass[]> {
+    //     this.tokenHeader = this.setTokenHeader();
+    //     return this.http.delete<TodoClass[]>(this.APIUrl + '/Todos/' + id, { headers: this.tokenHeader });
+    // }
+
+    // editTodos(id: number, dataToSend: TodoClass): Observable<TodoClass[]> {
+    //     return this.http.put<TodoClass[]>(this.APIUrl + '/Todos/' + id, dataToSend, { headers: this.tokenHeader });
+    // }
+
+    // completeTodo(id: number, val: TodoClass): Observable<TodoClass[]> {
+    //     this.tokenHeader = this.setTokenHeader();
+    //     return this.http.put<TodoClass[]>(this.APIUrl + '/Todos/' + id, val, { headers: this.tokenHeader });
+    // }
+
+    // registreUser(dataToSend: loginClass): Observable<loginClass[]> {
+    //     this.tokenHeader = this.setTokenHeader();
+    //     return this.http.post<loginClass[]>(this.loginUrl + '/register/', dataToSend, { headers: this.tokenHeader });
+    // }
+
+    // changeStatus(id: number, val: TodoClass): Observable<TodoClass[]> {
+    //     this.tokenHeader = this.setTokenHeader();
+    //     return this.http.put<TodoClass[]>(this.APIUrl + '/Todos/' + id, val, { headers: this.tokenHeader });
+    // }
+
+    // login(formData: loginClass): Observable<loginClass> {
+    //     return this.http.post<loginClass>(this.loginUrl + '/login/', {
+    //         email: formData.email,
+    //         password: formData.password
+    //     }, httpOptions);
+    // }
+
+
+//ttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
+    readonly APIUrl = "https://todoappbackend.azure-api.net/v1/api";
+
+    readonly loginUrl = "https://todoappbackend.azure-api.net/v1/Account";
 
     public $todos = new BehaviorSubject<TodoClass[]>([])
 
